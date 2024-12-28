aa2273843396d6c055c006140fc4740fec8cd7d5
LimeGradient <87611796+LimeGradient@users.noreply.github.com>
2024-12-27 20:39:52 -0500

    window title tweak

--- common/src/main/java/xyz/violaflower/legacy_tweaks/LegacyTweaks.java
+++ common/src/main/java/xyz/violaflower/legacy_tweaks/LegacyTweaks.java
@@ -3,6 +3,8 @@ package xyz.violaflower.legacy_tweaks;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 import xyz.violaflower.legacy_tweaks.items.ItemManager;
+import xyz.violaflower.legacy_tweaks.tweaks.TweakManager;
+import xyz.violaflower.legacy_tweaks.tweaks.impl.WindowTitle;
 
 public final class LegacyTweaks {
     public static final String MOD_ID = "legacy_tweaks";
@@ -11,5 +13,8 @@ public final class LegacyTweaks {
     public static void init() {
         LOGGER.info("initialized Legacy Tweaks successfully");
         ItemManager.init();
+
+        TweakManager tweakManager = TweakManager.getInstance();
+        tweakManager.register(new WindowTitle());
     }
 }
