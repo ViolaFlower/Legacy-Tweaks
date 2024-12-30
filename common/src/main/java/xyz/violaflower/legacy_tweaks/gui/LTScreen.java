c1aef3f34ead3bc620c99d15f17aaadb864a0f50
Jab125 <67534807+Jab125@users.noreply.github.com>
2024-12-30 16:41:59 +1100

    fix the remaining compile errors

--- common/src/main/java/xyz/violaflower/legacy_tweaks/gui/LTScreen.java
+++ common/src/main/java/xyz/violaflower/legacy_tweaks/gui/LTScreen.java
@@ -2,22 +2,19 @@ package xyz.violaflower.legacy_tweaks.gui;
 
 import net.fabricmc.api.EnvType;
 import net.fabricmc.api.Environment;
-import net.minecraft.client.gui.DrawContext;
-import net.minecraft.client.gui.Drawable;
-import net.minecraft.client.gui.screen.Screen;
-import net.minecraft.client.gui.widget.ButtonWidget;
-import net.minecraft.text.Text;
+
+import net.minecraft.client.gui.GuiGraphics;
+import net.minecraft.client.gui.components.Button;
+import net.minecraft.client.gui.screens.Screen;
+import net.minecraft.network.chat.Component;
 import xyz.violaflower.legacy_tweaks.tweaks.Tweak;
 import xyz.violaflower.legacy_tweaks.tweaks.TweakManager;
-
-import java.awt.*;
-import java.util.concurrent.atomic.AtomicInteger;
 
 @Environment(EnvType.CLIENT)
 public class LTScreen extends Screen {
 
     public LTScreen() {
-        super(Text.translatable("lt.screens.ltscreen.title"));
+        super(Component.translatable("lt.screens.ltscreen.title"));
     }
 
     @Override
@@ -26,12 +23,12 @@ public class LTScreen extends Screen {
         int i = 60;
         for (Tweak tweak : managerInstance.tweaks.values()) {
             int finalI = i;
-            this.addDrawable((context, mouseX, mouseY, delta) -> context.drawCenteredTextWithShadow(textRenderer, Text.literal(tweak.getTweakID() + " - " + tweak.getTweakAuthor()), width / 2, height / 2 - finalI, 0xffffff));
-            this.addDrawableChild(ButtonWidget.builder(Text.translatable(tweak.isEnabled() ? "lt.main.enabled" : "lt.main.disabled"), button -> {
+            this.addRenderableOnly((graphics, mouseX, mouseY, delta) -> graphics.drawCenteredString(font, Component.literal(tweak.getTweakID() + " - " + tweak.getTweakAuthor()), width / 2, height / 2 - finalI, 0xffffff));
+            this.addRenderableWidget(Button.builder(Component.translatable(tweak.isEnabled() ? "lt.main.enabled" : "lt.main.disabled"), button -> {
                 tweak.setEnabled(!tweak.isEnabled());
-                button.setMessage(Text.translatable(tweak.isEnabled() ? "lt.main.enabled" : "lt.main.disabled"));
+                button.setMessage(Component.translatable(tweak.isEnabled() ? "lt.main.enabled" : "lt.main.disabled"));
                 TweakManager.save();
-            }).dimensions(width / 2, height / 2 - i, 200, 20).build());
+            }).bounds(width / 2, height / 2 - i, 200, 20).build());
             i -= 40;
         }
     }
@@ -39,9 +36,9 @@ public class LTScreen extends Screen {
     // Dexrn: Made this screen really quickly, now we have something to work on.
     // Also, some of this is janky. (expected)
     @Override
-    public void render(DrawContext context, int mouseX, int mouseY, float delta) {
-        super.render(context, mouseX, mouseY, delta);
-        context.drawCenteredTextWithShadow(textRenderer, Text.translatable("lt.screens.ltscreen.title"), width / 2, height / 2 - 80, 0xffffff);
+    public void render(GuiGraphics graphics, int mouseX, int mouseY, float delta) {
+        super.render(graphics, mouseX, mouseY, delta);
+        graphics.drawCenteredString(font, Component.translatable("lt.screens.ltscreen.title"), width / 2, height / 2 - 80, 0xffffff);
 
         // oops apparently creating 20 buttons every frame :)))))))
 
