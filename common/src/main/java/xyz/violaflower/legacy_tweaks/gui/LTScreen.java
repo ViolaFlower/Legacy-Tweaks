3adb383fe56d58f63f493a5986035028c99f7dc3
Jab125 <67534807+Jab125@users.noreply.github.com>
2024-12-30 16:24:48 +1100

    fix screen

--- common/src/main/java/xyz/violaflower/legacy_tweaks/gui/LTScreen.java
+++ common/src/main/java/xyz/violaflower/legacy_tweaks/gui/LTScreen.java
@@ -3,9 +3,11 @@ package xyz.violaflower.legacy_tweaks.gui;
 import net.fabricmc.api.EnvType;
 import net.fabricmc.api.Environment;
 import net.minecraft.client.gui.DrawContext;
+import net.minecraft.client.gui.Drawable;
 import net.minecraft.client.gui.screen.Screen;
 import net.minecraft.client.gui.widget.ButtonWidget;
 import net.minecraft.text.Text;
+import xyz.violaflower.legacy_tweaks.tweaks.Tweak;
 import xyz.violaflower.legacy_tweaks.tweaks.TweakManager;
 
 import java.awt.*;
@@ -20,7 +22,17 @@ public class LTScreen extends Screen {
 
     @Override
     public void init() {
-
+        TweakManager managerInstance = TweakManager.getInstance();
+        int i = 60;
+        for (Tweak tweak : managerInstance.tweaks.values()) {
+            int finalI = i;
+            this.addDrawable((context, mouseX, mouseY, delta) -> context.drawCenteredTextWithShadow(textRenderer, Text.literal(tweak.getTweakID() + " - " + tweak.getTweakAuthor()), width / 2, height / 2 - finalI, 0xffffff));
+            this.addDrawableChild(ButtonWidget.builder(Text.literal(tweak.isEnabled() ? "Enabled" : "Disabled"), button -> {
+                tweak.setEnabled(!tweak.isEnabled());
+                button.setMessage(Text.literal(tweak.isEnabled() ? "Enabled" : "Disabled"));
+            }).dimensions(width / 2, height / 2 - i, 200, 20).build());
+            i -= 40;
+        }
     }
 
     // Dexrn: Made this screen really quickly, now we have something to work on.
@@ -29,18 +41,8 @@ public class LTScreen extends Screen {
     public void render(DrawContext context, int mouseX, int mouseY, float delta) {
         super.render(context, mouseX, mouseY, delta);
         context.drawCenteredTextWithShadow(textRenderer, Text.translatable("lt.screens.ltscreen.title"), width / 2, height / 2 - 80, 0xffffff);
-        TweakManager managerInstance = TweakManager.getInstance();
-        AtomicInteger i = new AtomicInteger(60);
 
         // oops apparently creating 20 buttons every frame :)))))))
-        managerInstance.tweaks.forEach((id, tweak) -> {
-            context.drawCenteredTextWithShadow(textRenderer, Text.literal(tweak.getTweakID() + " - " + tweak.getTweakAuthor()), width / 2, height / 2 - i.get(), 0xffffff);
 
-            this.addDrawableChild(ButtonWidget.builder(Text.literal(tweak.isEnabled() ? "Enabled" : "Disabled"), button -> {
-                tweak.setEnabled(!tweak.isEnabled());
-            }).dimensions(width / 2, height / 2 - (i.get()), 200, 20).build());
-
-            i.addAndGet(-40);
-        });
     }
 }
