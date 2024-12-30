f02b70534c2ba596d580304a4ca8d6b74ecef499
Jab125 <67534807+Jab125@users.noreply.github.com>
2024-12-30 17:44:38 +1100

    Update MapRendererMixin.java

--- src/main/java/xyz/violaflower/legacy_tweaks/mixin/MapRendererMixin.java
+++ src/main/java/xyz/violaflower/legacy_tweaks/mixin/MapRendererMixin.java
@@ -6,6 +6,7 @@ import net.minecraft.client.gui.Font;
 import net.minecraft.client.gui.MapRenderer;
 import net.minecraft.client.renderer.MultiBufferSource;
 import org.spongepowered.asm.mixin.Mixin;
+import org.spongepowered.asm.mixin.Unique;
 import org.spongepowered.asm.mixin.injection.At;
 import org.spongepowered.asm.mixin.injection.Inject;
 import org.spongepowered.asm.mixin.injection.callback.CallbackInfo;
@@ -17,16 +18,21 @@ public class MapRendererMixin {
 	public static class Instance {
 		@Inject(method = "draw", at = @At("HEAD"))
 		void injectDraw(PoseStack arg, MultiBufferSource bufferSource, boolean active, int packedLight, CallbackInfo ci) {
-			if (TweakManager.getInstance().getTweak("mapcoords").isEnabled()) {
+			if (mapChanges()) {
 				Minecraft minecraft = Minecraft.getInstance();
 				arg.pushPose();
 				arg.translate(0, 0, -0.1);
+				assert minecraft.player != null; // why would this be null
 				int x = minecraft.player.getBlockX();
 				int y = minecraft.player.getBlockY();
 				int z = minecraft.player.getBlockZ();
-				minecraft.font.drawInBatch("X:%s Y:%s Z:%s".formatted(x, y, z), 0, 0, 0, false, arg.last().pose(), bufferSource, Font.DisplayMode.NORMAL, 0, packedLight);
+				minecraft.font.drawInBatch("X:%s Y:%s Z:%s".formatted(x, y, z), /* I have no idea what these 3 numbers are */ 0, 0, 0, false, arg.last().pose(), bufferSource, Font.DisplayMode.NORMAL, 0, packedLight);
 				arg.popPose();
 			}
+		}
+		@Unique
+		private static boolean mapChanges() {
+			return TweakManager.getInstance().getTweak("mapcoords").isEnabled();
 		}
 	}
 }
