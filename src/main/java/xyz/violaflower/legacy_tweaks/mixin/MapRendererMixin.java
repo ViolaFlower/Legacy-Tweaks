3575b684290d5cd26de58ee903902522bdfc2293
Jab125 <67534807+Jab125@users.noreply.github.com>
2024-12-30 18:56:12 +1100

    fix, i think?

--- src/main/java/xyz/violaflower/legacy_tweaks/mixin/MapRendererMixin.java
+++ src/main/java/xyz/violaflower/legacy_tweaks/mixin/MapRendererMixin.java
@@ -12,27 +12,24 @@ import org.spongepowered.asm.mixin.injection.Inject;
 import org.spongepowered.asm.mixin.injection.callback.CallbackInfo;
 import xyz.violaflower.legacy_tweaks.tweaks.TweakManager;
 
-@Mixin(MapRenderer.class)
+@Mixin(targets = "net/minecraft/client/gui/MapRenderer$MapInstance")
 public class MapRendererMixin {
-	@Mixin(targets = "net/minecraft/client/gui/MapRenderer$MapInstance")
-	public static class Instance {
-		@Inject(method = "draw", at = @At("HEAD"))
-		void injectDraw(PoseStack arg, MultiBufferSource bufferSource, boolean active, int packedLight, CallbackInfo ci) {
-			if (mapChanges()) {
-				Minecraft minecraft = Minecraft.getInstance();
-				arg.pushPose();
-				arg.translate(0, 0, -0.1);
-				assert minecraft.player != null; // why would this be null
-				int x = minecraft.player.getBlockX();
-				int y = minecraft.player.getBlockY();
-				int z = minecraft.player.getBlockZ();
-				minecraft.font.drawInBatch("X:%s Y:%s Z:%s".formatted(x, y, z), /* I have no idea what these 3 numbers are */ 0, 0, 0, false, arg.last().pose(), bufferSource, Font.DisplayMode.NORMAL, 0, packedLight);
-				arg.popPose();
-			}
+	@Inject(method = "draw", at = @At("HEAD"))
+	private void injectDraw(PoseStack arg, MultiBufferSource bufferSource, boolean active, int packedLight, CallbackInfo ci) {
+		if (mapChanges()) {
+			Minecraft minecraft = Minecraft.getInstance();
+			arg.pushPose();
+			arg.translate(0, 0, -0.1);
+			assert minecraft.player != null; // why would this be null
+			int x = minecraft.player.getBlockX();
+			int y = minecraft.player.getBlockY();
+			int z = minecraft.player.getBlockZ();
+			minecraft.font.drawInBatch("X:%s Y:%s Z:%s".formatted(x, y, z), /* I have no idea what these 3 numbers are */ 0, 0, 0, false, arg.last().pose(), bufferSource, Font.DisplayMode.NORMAL, 0, packedLight);
+			arg.popPose();
 		}
-		@Unique
-		private static boolean mapChanges() {
-			return TweakManager.getInstance().getTweak("mapcoords").isEnabled();
-		}
+	}
+	@Unique
+	private static boolean mapChanges() {
+		return TweakManager.getInstance().getTweak("mapcoords").isEnabled();
 	}
 }
