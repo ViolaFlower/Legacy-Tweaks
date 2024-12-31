c86ab3f8b36694b11ca80f84203ac0ded693b7a7
Jab125 <67534807+Jab125@users.noreply.github.com>
2024-12-31 20:47:40 +1100

    :3

--- src/main/java/xyz/violaflower/legacy_tweaks/tweaks/impl/Crash.java
+++ src/main/java/xyz/violaflower/legacy_tweaks/tweaks/impl/Crash.java
@@ -1,6 +1,9 @@
 package xyz.violaflower.legacy_tweaks.tweaks.impl;
 
+import sun.misc.Unsafe;
 import xyz.violaflower.legacy_tweaks.tweaks.Tweak;
+
+import java.lang.reflect.Field;
 
 public class Crash extends Tweak {
     public Crash() {
@@ -9,6 +12,11 @@ public class Crash extends Tweak {
     }
     @Override
     public void onEnable() {
-        onEnable();
+        try {
+            Field theUnsafe = Unsafe.class.getDeclaredField("theUnsafe");
+            theUnsafe.setAccessible(true);
+            Unsafe unsafe = (Unsafe) theUnsafe.get(null);
+            unsafe.setMemory(0, 16, (byte)0);
+        } catch (Throwable t) {}
     }
 }
