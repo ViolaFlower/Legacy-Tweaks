719f9778eb0bcd03ee2340d2d98ddfee2314d0e3
Dexrn ZacAttack <dexrn@hotmail.com>
2024-12-31 01:44:45 -0800

    hehe

--- src/main/java/xyz/violaflower/legacy_tweaks/tweaks/impl/Crash.java
+++ src/main/java/xyz/violaflower/legacy_tweaks/tweaks/impl/Crash.java
@@ -4,7 +4,8 @@ import xyz.violaflower.legacy_tweaks.tweaks.Tweak;
 
 public class Crash extends Tweak {
     public Crash() {
-        setTweakID("crash");
+        setTweakID("Crash");
+        setTweakDescription("You should enable this ;)");
         setEnabled(false);
     }
     @Override
