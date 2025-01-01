a4e3b9f7b24a1b618aa1c4309a23c2843c7d5349
Jab125 <67534807+Jab125@users.noreply.github.com>
2025-01-01 22:57:04 +1100

    fix neoforge

--- src/main/java/xyz/violaflower/legacy_tweaks/client/LegacyTweaksClient.java
+++ src/main/java/xyz/violaflower/legacy_tweaks/client/LegacyTweaksClient.java
@@ -1,18 +1,24 @@
 package xyz.violaflower.legacy_tweaks.client;
 
-import net.fabricmc.fabric.api.resource.IdentifiableResourceReloadListener;
-import net.fabricmc.fabric.api.resource.ResourceManagerHelper;
+//? if fabric {
 import net.minecraft.resources.ResourceLocation;
 import net.minecraft.server.packs.PackType;
+import net.fabricmc.fabric.api.resource.IdentifiableResourceReloadListener;
+import net.fabricmc.fabric.api.resource.ResourceManagerHelper;
 import net.minecraft.server.packs.resources.ResourceManager;
 import net.minecraft.util.profiling.ProfilerFiller;
 import xyz.violaflower.legacy_tweaks.LegacyTweaks;
 
 import java.util.concurrent.CompletableFuture;
 import java.util.concurrent.Executor;
+//?} elif neoforge {
+/*import net.minecraft.client.Minecraft;
+import net.minecraft.server.packs.resources.ReloadableResourceManager;
+*///?}
 
 public class LegacyTweaksClient {
 	public static void init() {
+		//? if fabric {
 		ResourceManagerHelper.get(PackType.CLIENT_RESOURCES).registerReloadListener(new IdentifiableResourceReloadListener() {
 			private final LegacyTweaksResourceManager legacyTweaksResourceManager = new LegacyTweaksResourceManager();
 			@Override
@@ -30,5 +36,7 @@ public class LegacyTweaksClient {
 				return legacyTweaksResourceManager.reload(preparationBarrier, resourceManager, profilerFiller,profilerFiller2, executor, executor2);
 			}
 		});
+		//?} elif neoforge
+		/*((ReloadableResourceManager) Minecraft.getInstance().getResourceManager()).registerReloadListener(new LegacyTweaksResourceManager());*/
 	}
 }
