5058575ba448b10f04910d3e499eda1c908b0efe
Jab125 <67534807+Jab125@users.noreply.github.com>
2025-01-03 19:06:04 +1100

    add legacy water tweak (non-functional)

--- src/main/java/xyz/violaflower/legacy_tweaks/tweaks/impl/EyeCandy.java
+++ src/main/java/xyz/violaflower/legacy_tweaks/tweaks/impl/EyeCandy.java
@@ -9,6 +9,7 @@ public class EyeCandy extends Tweak {
 	public final LegacyTextShadows legacyTextShadows;
 	public final FineTunedUIScale fineTunedUIScale;
 	public final Tweak oldButton;
+	public final Tweak legacyWaterColors;
 
 	public EyeCandy() {
 		setTweakID("Eye Candy");
@@ -18,6 +19,7 @@ public class EyeCandy extends Tweak {
 		addSubTweak(legacyTextShadows = new LegacyTextShadows());
 		addSubTweak(fineTunedUIScale = new FineTunedUIScale());
 		addSubTweak(oldButton = new TweakBuilder("Pre 1.13 button").description("Reverts buttons to their pre 1.14 state").authors("Jab125").setDefaultEnabled(true).build());
+		addSubTweak(legacyWaterColors = new TweakBuilder("Legacy Water Colors").description("앚졸딩도알lk노wn라w소f아v이앋이온텔에잇노왜압에엣호울d베압렏오f뤼잊윙살엗오옷말l독엗잊f앋릳t렙옫요ff텍로운d텝에어f조울세f리엣안위왭엦아웃엡에엣돈t잘에w핱움안s틴깃임봈입레").authors("Jab125", "dexrn").setDefaultEnabled(true).build());
 	}
 	public static class LegacyTextShadows extends Tweak {
 		public final IntSliderOption shadowOffset;
