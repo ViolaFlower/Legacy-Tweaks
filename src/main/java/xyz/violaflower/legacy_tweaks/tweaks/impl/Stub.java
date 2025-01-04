ae546cd7ebeacf692d55ce79ee08926f98da223e
Jab125 <67534807+Jab125@users.noreply.github.com>
2025-01-04 16:59:12 +1100

    add example to stub

--- src/main/java/xyz/violaflower/legacy_tweaks/tweaks/impl/Stub.java
+++ src/main/java/xyz/violaflower/legacy_tweaks/tweaks/impl/Stub.java
@@ -1,11 +1,16 @@
 package xyz.violaflower.legacy_tweaks.tweaks.impl;
 
+import net.minecraft.ChatFormatting;
+import net.minecraft.network.chat.Component;
+import net.minecraft.world.item.Rarity;
 import xyz.violaflower.legacy_tweaks.tweaks.Tweak;
 
 public class Stub extends Tweak {
     public Stub() {
-        setTweakAuthor("DexrnZacAttack");
+        setTweakAuthor("DexrnZacAttack", "Jab125");
         setTweakDescription("This does absolutely nothing.");
         setTweakID("StubTweak");
+        addSliderOption("Chat Component", enumProvider(ChatFormatting.RED, ChatFormatting::values, ChatFormatting::getName, f -> Component.literal(f.getName()).withStyle(f)));
+        addSliderOption("Rarity", enumProvider(Rarity.COMMON, Rarity::values, Rarity::getSerializedName, f -> Component.literal(f.getSerializedName()).withStyle(f.color())));
     }
 }
