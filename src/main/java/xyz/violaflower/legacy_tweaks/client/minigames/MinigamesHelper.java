197a7747631e6e79664360fb8e7b42a5576a50d3
Jab125 <67534807+Jab125@users.noreply.github.com>
2025-01-04 21:48:47 +1100

    add stub minigames helper

---
+++ src/main/java/xyz/violaflower/legacy_tweaks/client/minigames/MinigamesHelper.java
@@ -0,0 +1,16 @@
+package xyz.violaflower.legacy_tweaks.client.minigames;
+
+// TODO: Everything. This probably won't be done in time for the initial release.
+public class MinigamesHelper {
+	public static boolean isInGlideMinigame() {
+		return false;
+	}
+
+	public static boolean isInTumbleMinigame() {
+		return false;
+	}
+
+	public static boolean isInBattleMinigame() {
+		return false;
+	}
+}
