fefacfbbc0aca469fcca0c89a3c002ce74f7bd6c
Jab125 <67534807+Jab125@users.noreply.github.com>
2025-01-06 17:23:20 +1100

    implement InternalClientConfigurationEvents.START

--- src/main/java/xyz/violaflower/legacy_tweaks/networking/configuration/event/ClientConfigurationEvents.java
+++ src/main/java/xyz/violaflower/legacy_tweaks/networking/configuration/event/ClientConfigurationEvents.java
@@ -2,10 +2,11 @@ package xyz.violaflower.legacy_tweaks.networking.configuration.event;
 
 //? if fabric {
 import net.fabricmc.fabric.api.client.networking.v1.ClientConfigurationConnectionEvents;
-//?}
+//?} elif neoforge {
+/*import xyz.violaflower.legacy_tweaks.networking.event.InternalClientConfigurationEvents;
+*///?}
 import net.minecraft.client.Minecraft;
 import net.minecraft.client.multiplayer.ClientConfigurationPacketListenerImpl;
-import xyz.violaflower.legacy_tweaks.networking.event.InternalClientConfigurationEvents;
 
 public class ClientConfigurationEvents {
 	public static void registerOnStart(Start start) {
