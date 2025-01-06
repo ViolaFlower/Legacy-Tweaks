12dcc5b6a1d72d473189e177758054ff735b0c72
Jab125 <67534807+Jab125@users.noreply.github.com>
2025-01-06 18:05:44 +1100

    add test configuration packet

--- src/main/java/xyz/violaflower/legacy_tweaks/network/Networking.java
+++ src/main/java/xyz/violaflower/legacy_tweaks/network/Networking.java
@@ -1,25 +1,38 @@
 package xyz.violaflower.legacy_tweaks.network;
 
+import net.minecraft.network.protocol.common.custom.CustomPacketPayload;
+import net.minecraft.server.network.ConfigurationTask;
 import xyz.violaflower.legacy_tweaks.network.client.ClientNetworking;
+import xyz.violaflower.legacy_tweaks.network.payload.CoolConfigurationPacket;
 import xyz.violaflower.legacy_tweaks.network.payload.CoolPacket;
 import xyz.violaflower.legacy_tweaks.network.payload.CoolPacket2;
+import xyz.violaflower.legacy_tweaks.network.task.SendCoolConfigurationPacketTask;
 import xyz.violaflower.legacy_tweaks.networking.NetworkingAbstractions;
+import xyz.violaflower.legacy_tweaks.networking.configuration.ServerboundConfiguration;
+import xyz.violaflower.legacy_tweaks.networking.configuration.event.ServerConfigurationEvents;
 import xyz.violaflower.legacy_tweaks.networking.play.ServerboundPlay;
+
+import java.util.function.Consumer;
 
 public class Networking {
 	public static void registerNetworkingCodecs() {
 		NetworkingAbstractions.registerCodec(CoolPacket.TYPE, CoolPacket.STREAM_CODEC, NetworkingAbstractions.PayloadType.PLAY_S2C);
 		NetworkingAbstractions.registerCodec(CoolPacket2.TYPE, CoolPacket2.STREAM_CODEC, NetworkingAbstractions.PayloadType.PLAY_C2S);
+		NetworkingAbstractions.registerCodec(CoolConfigurationPacket.TYPE, CoolConfigurationPacket.STREAM_CODEC, NetworkingAbstractions.PayloadType.CONFIGURATION_S2C);
+		NetworkingAbstractions.registerCodec(CoolConfigurationPacket.TYPE, CoolConfigurationPacket.STREAM_CODEC, NetworkingAbstractions.PayloadType.CONFIGURATION_C2S);
 	}
 
 	public static void registerPayloadHandlers() {
 		ServerboundPlay.registerHandler(CoolPacket2.TYPE, (payload, context) -> {
 			System.out.println("[SERVER] received packet " + payload);
 		});
+		ServerboundConfiguration.registerHandler(CoolConfigurationPacket.TYPE, (payload, context) -> {
+			System.out.println("Client responded with " + payload + "!");
+		});
 	}
 
 	/// @see ClientNetworking#registerConfigurationTasks()
 	public static void registerConfigurationTasks() {
-
+		ServerConfigurationEvents.registerOnConfigure(new SendCoolConfigurationPacketTask());
 	}
 }
--- src/main/java/xyz/violaflower/legacy_tweaks/network/client/ClientNetworking.java
+++ src/main/java/xyz/violaflower/legacy_tweaks/network/client/ClientNetworking.java
@@ -1,17 +1,25 @@
 package xyz.violaflower.legacy_tweaks.network.client;
 
 import xyz.violaflower.legacy_tweaks.network.Networking;
+import xyz.violaflower.legacy_tweaks.network.payload.CoolConfigurationPacket;
 import xyz.violaflower.legacy_tweaks.network.payload.CoolPacket;
 import xyz.violaflower.legacy_tweaks.networking.configuration.ClientboundConfiguration;
 import xyz.violaflower.legacy_tweaks.networking.play.ClientboundPlay;
 
 public class ClientNetworking {
 	public static void registerPayloadHandlers() {
-		ClientboundPlay.registerHandler(CoolPacket.TYPE, (handler, context) -> {
-			System.out.println("RECEIVED!: " + handler.number() + " " + context.player());
+		ClientboundPlay.registerHandler(CoolPacket.TYPE, (payload, context) -> {
+			System.out.println("RECEIVED!: " + payload.number() + " " + context.player());
+		});
+
+		ClientboundConfiguration.registerHandler(CoolConfigurationPacket.TYPE, (payload, context) -> {
+			System.out.println("Received during configuration: " + payload);
+			System.out.println("Responding to server with the same payload...");
+			context.sendPacket(payload);
 		});
 	}
 
+	/// Note that configuration tasks for the client aren't finished.
 	/// @see Networking#registerConfigurationTasks()
 	public static void registerConfigurationTasks() {
 
