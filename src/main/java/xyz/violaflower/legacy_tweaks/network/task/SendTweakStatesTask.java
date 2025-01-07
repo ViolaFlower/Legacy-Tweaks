2e8a9dcb6b90ffbfd67eee78d42e9fafe3bd88b3
Jab125 <67534807+Jab125@users.noreply.github.com>
2025-01-07 19:00:07 +1100

    i forgot

--- src/main/java/xyz/violaflower/legacy_tweaks/network/task/SendTweakStatesTask.java
+++ src/main/java/xyz/violaflower/legacy_tweaks/network/task/SendTweakStatesTask.java
@@ -4,6 +4,7 @@ import net.minecraft.network.protocol.common.custom.CustomPacketPayload;
 import xyz.violaflower.legacy_tweaks.network.payload.CoolConfigurationPacket;
 import xyz.violaflower.legacy_tweaks.network.payload.TweakStatesPayload;
 import xyz.violaflower.legacy_tweaks.networking.configuration.event.ServerConfigurationEvents;
+import xyz.violaflower.legacy_tweaks.tweaks.TweakManager;
 import xyz.violaflower.legacy_tweaks.tweaks.TweakState;
 import xyz.violaflower.legacy_tweaks.util.ModAsset;
 
@@ -13,7 +14,7 @@ public class SendTweakStatesTask implements ServerConfigurationEvents.CustomPayl
 	public static final Type TYPE = new Type(ModAsset.getResourceLocation("send_tweak_states_task").toString());
 	@Override
 	public void run(Consumer<CustomPacketPayload> sender) {
-		sender.accept(new TweakStatesPayload(TweakState.encodeStates()));
+		sender.accept(new TweakStatesPayload(TweakManager.version, TweakState.encodeStates()));
 	}
 
 	@Override
