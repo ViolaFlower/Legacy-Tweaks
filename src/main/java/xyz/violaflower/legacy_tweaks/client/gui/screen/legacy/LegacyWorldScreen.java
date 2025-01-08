50cf256183ceb7460738963858b3cc55cf967ff8
Dexrn ZacAttack <dexrn@hotmail.com>
2025-01-07 23:55:16 -0800

    more screen stuff
    Co-Authored-By: Permdog99 <34215218+Permdog99@users.noreply.github.com>

--- src/main/java/xyz/violaflower/legacy_tweaks/client/gui/screen/legacy/LegacyWorldScreen.java
+++ src/main/java/xyz/violaflower/legacy_tweaks/client/gui/screen/legacy/LegacyWorldScreen.java
@@ -5,19 +5,20 @@ import net.minecraft.client.gui.components.Button;
 import net.minecraft.client.gui.layouts.FrameLayout;
 import net.minecraft.client.gui.layouts.LinearLayout;
 import net.minecraft.client.gui.screens.Screen;
+import net.minecraft.client.gui.screens.worldselection.SelectWorldScreen;
 import net.minecraft.network.chat.Component;
+import net.minecraft.world.level.storage.LevelStorageSource;
 import xyz.violaflower.legacy_tweaks.client.gui.element.LegacyLogoRenderer;
 import xyz.violaflower.legacy_tweaks.util.client.ScreenUtil;
 import xyz.violaflower.legacy_tweaks.util.common.assets.Sprites;
 
 import java.util.Objects;
+import java.util.concurrent.ExecutionException;
 
-// playground for testing gui things
 public class LegacyWorldScreen extends LegacyScreen {
     private final Screen parent;
     private final FrameLayout frameLayout;
     private final FrameLayout innerFrameLayout;
-    private LegacyLogoRenderer logoRenderer;
 
     protected LegacyWorldScreen(Screen parent) {
         super(Component.empty());
@@ -25,9 +26,8 @@ public class LegacyWorldScreen extends LegacyScreen {
         frameLayout = new FrameLayout(getFrameWidth(), getFrameHeight());
         //frameLayout.newChildLayoutSettings().alignVerticallyTop();
         frameLayout.defaultChildLayoutSetting().align(0.5f, 0).padding(5);
-        innerFrameLayout = new FrameLayout(getFrameWidth() - 20, getFrameHeight() - 20);
+        innerFrameLayout = new FrameLayout(getFrameWidth() - 23, getFrameHeight() - 23);
         innerFrameLayout.defaultChildLayoutSetting().alignVerticallyTop().padding(5);
-        this.logoRenderer = Objects.requireNonNullElseGet(logoRenderer, () -> new LegacyLogoRenderer(false));
     }
 
     @Override
@@ -57,13 +57,29 @@ public class LegacyWorldScreen extends LegacyScreen {
     @Override
     protected void init() {
         super.init();
-        LinearLayout linearLayout = LinearLayout.vertical().spacing(getButtonSpacing());
-        linearLayout.addChild(Button.builder(Component.literal("Button 1"), button -> {}).size(getButtonWidth(), getButtonHeight()).build());
-        linearLayout.addChild(Button.builder(Component.literal("Button 2"), button -> {}).size(getButtonWidth(), getButtonHeight()).build());
-        linearLayout.addChild(Button.builder(Component.literal("Button 3"), button -> {}).size(getButtonWidth(), getButtonHeight()).build());
-        linearLayout.addChild(Button.builder(Component.literal("Button 4"), button -> {}).size(getButtonWidth(), getButtonHeight()).build());
+        LevelStorageSource.LevelCandidates cd = this.minecraft.getLevelSource().findLevelCandidates();
+        java.util.List<net.minecraft.world.level.storage.LevelSummary> summaries = null;
+        try {
+            summaries = this.minecraft.getLevelSource().loadLevelSummaries(cd).get();
+        } catch (InterruptedException e) {
+            throw new RuntimeException(e);
+        } catch (ExecutionException e) {
+            throw new RuntimeException(e);
+        }
+        LinearLayout linearLayout = LinearLayout.vertical().spacing(4);
+        {
+            LinearLayout layout = LinearLayout.vertical().spacing(0);
+            summaries.forEach(shart -> {
+                // Should have the icon on the side of the button... can't do that yet.
+                layout.addChild(Button.builder(Component.literal(shart.getLevelName()), button -> {
+                    // TODO: ALLOW LOADING WORLD
+                }).size(getButtonWidthAlt(), getButtonHeight()).build());
+            });
+            innerFrameLayout.addChild(layout);
+        }
 
-        innerFrameLayout.addChild(linearLayout);
+        linearLayout.addChild(innerFrameLayout);
+        frameLayout.addChild(linearLayout);
         frameLayout.visitWidgets(this::addRenderableWidget);
         repositionElements();
     }
@@ -76,11 +92,7 @@ public class LegacyWorldScreen extends LegacyScreen {
 
     @Override
     public int getButtonWidth() {
-        if (ScreenUtil.isLargeGui()) {
-            return 202;
-        } else {
-            return 270;
-        }
+        return getFrameWidth() - 23 - 5;
     }
 
     public int getButtonWidthAlt() {
@@ -103,7 +115,7 @@ public class LegacyWorldScreen extends LegacyScreen {
         guiGraphics.blitSprite(Sprites.TOP_NAV_LEFT, frameLayout.getX() + frameLayout.getWidth() / 3, frameLayout.getY() - 15, frameLayout.getWidth() / 3, 19);
         guiGraphics.blitSprite(Sprites.TOP_NAV_LEFT, frameLayout.getX() + frameLayout.getWidth() / 3 * 2, frameLayout.getY() - 15, frameLayout.getWidth() / 3, 19);
         guiGraphics.blitSprite(Sprites.BACKGROUND_LOC, frameLayout.getX(), frameLayout.getY(), frameLayout.getWidth(), frameLayout.getHeight());
-        guiGraphics.blitSprite(Sprites.INSET_BACKGROUND, frameLayout.getX() + 9, frameLayout.getY() + 9, innerFrameLayout.getWidth(), innerFrameLayout.getHeight());
+        guiGraphics.blitSprite(Sprites.INSET_BACKGROUND, frameLayout.getX() + 5, frameLayout.getY() + 5, innerFrameLayout.getWidth() + 9, innerFrameLayout.getHeight() + 9);
         guiGraphics.blitSprite(Sprites.TOP_NAV_LEFT, frameLayout.getX(), frameLayout.getY() - 15, frameLayout.getWidth() / 3, 19);
     }
 
@@ -111,6 +123,5 @@ public class LegacyWorldScreen extends LegacyScreen {
     public void render(GuiGraphics guiGraphics, int mouseX, int mouseY, float partialTick) {
         this.renderPanorama(guiGraphics, partialTick);
         super.render(guiGraphics, mouseX, mouseY, partialTick);
-        logoRenderer.renderLogo(guiGraphics, this.width, 1);
     }
 }
