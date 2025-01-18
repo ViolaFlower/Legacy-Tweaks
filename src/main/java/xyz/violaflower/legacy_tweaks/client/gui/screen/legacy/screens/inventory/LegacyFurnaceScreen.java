43c49607358319c1d927be67c29e99fa2e898bdc
Permdog99 <34215218+Permdog99@users.noreply.github.com>
2025-01-18 02:40:14 -0500

    Removed env type from LegacyFurnaceScreen to fix NeoForge

--- src/main/java/xyz/violaflower/legacy_tweaks/client/gui/screen/legacy/screens/inventory/LegacyFurnaceScreen.java
+++ src/main/java/xyz/violaflower/legacy_tweaks/client/gui/screen/legacy/screens/inventory/LegacyFurnaceScreen.java
@@ -1,8 +1,5 @@
 package xyz.violaflower.legacy_tweaks.client.gui.screen.legacy.screens.inventory;
 
-import com.mojang.datafixers.util.Pair;
-import net.fabricmc.api.EnvType;
-import net.fabricmc.api.Environment;
 import net.minecraft.client.gui.screens.recipebook.SmeltingRecipeBookComponent;
 import net.minecraft.network.chat.Component;
 import net.minecraft.resources.ResourceLocation;
@@ -14,7 +11,6 @@ import xyz.violaflower.legacy_tweaks.client.gui.extention.SlotExtension;
 import xyz.violaflower.legacy_tweaks.util.common.assets.ModAsset;
 import xyz.violaflower.legacy_tweaks.util.common.assets.Sprites;
 
-@Environment(EnvType.CLIENT)
 public class LegacyFurnaceScreen extends LegacyAbstractFurnaceScreen<FurnaceMenu> {
 	private static final ResourceLocation LIT_PROGRESS_SPRITE = ResourceLocation.withDefaultNamespace("container/furnace/lit_progress");
 	private static final ResourceLocation BURN_PROGRESS_SPRITE = ResourceLocation.withDefaultNamespace("container/furnace/burn_progress");
