5ca99b60f2164ea93dd83fcbcce68cedf996e232
Jab125 <67534807+Jab125@users.noreply.github.com>
2025-01-19 21:16:00 +1100

    Create VirtualInventory.java

---
+++ src/main/java/xyz/violaflower/legacy_tweaks/client/gui/extention/VirtualInventory.java
@@ -0,0 +1,15 @@
+package xyz.violaflower.legacy_tweaks.client.gui.extention;
+
+import net.minecraft.world.inventory.Slot;
+
+public interface VirtualInventory extends Iterable<Slot> {
+	// from 0 to 36
+	Slot getSlot(int slot);
+	// from 0 to 27
+	Slot getInventorySlot(int slot);
+	// from 0 to 9
+	Slot getHotbarSlot(int slot);
+	default Iterable<Slot> iterateSlots() { return this; }
+	Iterable<Slot> iterateInventorySlots();
+	Iterable<Slot> iterateHotbarSlots();
+}
